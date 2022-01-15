3846a6f98f8ede0217e3fefa8c6fca525e218113
Paloma Lins <palomalimadl@gmail.com>
2022-01-15 09:30:01 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let idade = 18;
+if (idade>=18){
+    console.log("Você é maior de idade")
+}
+let souHumana = true;
+if(idade>=18 && souHumana == true){
+    console.log("Você é um humano maior de idade")
+}
+let janeiro = true;
+let dezembro= false;
+if(janeiro == true && dezembro == false){
+    console.log("Parabéns por nascer no primeiro mês do ano")
+}else{
+    console.log("Parabéns por nascer no último mês do ano")
+}
+let ferreira = true;
+let lins = false;
+if(ferreira == true && lins == false){
+    console.log("Seu sobrenome é extenso!")
+} else{
+    console.log("Seu sobrenome é mínimo")
+}
+let enzzo = true;
+let henrique = false;
+if(enzzo == true && henrique == false){
+    console.log("Seu nome começa com E!")
+} else{
+    console.log("Seu nome não começa com E")
+}
